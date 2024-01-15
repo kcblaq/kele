f96cdba43db91b3ac1852efaa25c9000cd572e8f
Kelechi Ugwu <kcblack22@gmail.com>
2024-01-15 13:02:28 +0100

    Restored li

--- src/components/projects/GlobalReformation.tsx
+++ src/components/projects/GlobalReformation.tsx
@@ -9,13 +9,13 @@ export default function GlobalReformation({url}: Props) {
     const description = <p>
        An e-learning platform for global leadership transformation.
        It is a full-featured platform for e-learning, including;
-       <ul>
-        <li>- Multi-user category for admin, students and tutors</li>
-        <li> - Registration and login</li>
-        <li>- Taking courses and quizes</li>
-        <li> - Uploading and scoring quizes</li>
-        <li> - Certificate generation after a succesfull course.</li>
-       </ul>
+       {/* <ul> */}
+        <li> Multi-user category for admin, students and tutors</li>
+        <li>  Registration and login</li>
+        <li> Taking courses and quizes</li>
+        <li>  Uploading and scoring quizes</li>
+        <li>  Certificate generation after a succesfull course.</li>
+       {/* </ul> */}
     </p>
   return (
     <Layout title={'Global Reformation'} description={description} link={url} />
--- src/components/projects/Haloasset.tsx
+++ src/components/projects/Haloasset.tsx
@@ -8,14 +8,14 @@ export default function Haloasset({url}: Prop) {
   const description = <p>
 Halo Asset is a fintech platform where verified users can save, invest and borrow money.
 Users can;
-<ul>
-  <li> - Create account and get verified </li>
-  <li> - Send and recieve money on their wallets</li>
-  <li>- Save in various forms, such as automated deduction from a linked card periodically</li>
-  <li>-  Invest into various stocks</li> 
-  <li>-  Withdraw into an account </li> 
+
+  <li>  Create account and get verified </li>
+  <li>  Send and recieve money on their wallets</li>
+  <li> Save in various forms, such as automated deduction from a linked card periodically</li>
+  <li>  Invest into various stocks</li> 
+  <li>  Withdraw into an account </li> 
   etc.
-</ul>
+
     </p>
   return (
     <Layout title={'Hallo Asset'} description={description} link={url} />
