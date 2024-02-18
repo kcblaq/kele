adf34e868b01218e02a632ccd0b7cb0a27b6b478
Kelechi Ugwu <kcblack22@gmail.com>
2024-02-18 17:38:49 +0100

    Mobile menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,11 @@ import { PiFigmaLogoLight } from "react-icons/pi";
 import { SiPostgresql } from "react-icons/si";
 import { SiPrisma } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
-import { IoMenuSharp } from "react-icons/io5";
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+
+
+
 
 
 import Head from 'next/head'
@@ -23,111 +27,153 @@ import Head from 'next/head'
 // import { IoLogoJavascript } from "react-icons/io";
 
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Welcome from "@/components/Welcome";
 import GlobalReformation from "@/components/projects/GlobalReformation";
 import Kejepay from "@/components/projects/Kejepay";
 import Shortchase from "@/components/projects/Shortchase";
 import Zummit from "@/components/projects/Zummit";
 import Haloasset from "@/components/projects/Haloasset";
+import Link from "next/link";
 
 
 export default function Home() {
-const [current, setCurrent] = useState('Welcome');
+  const [current, setCurrent] = useState('Welcome');
+  const [showMenu, setShowMenu] = useState(false)
+  const projects = [{ name: 'Shortchase', link: 'https://shortchaseclient.netlify.app/' },
+  { name: 'Zummit', link: 'https://zummitafrica.com/' }, { name: 'Hallo', link: 'https://www.haloasset.com/' },
+  { name: 'GlobalReformation', link: 'https://globalreformationnetwork.org/' }]
+  const toolset = [{ name: 'Javascript', icon: <TbBrandJavascript /> }, { name: 'ReactJS', icon: <FaReact /> }, { name: 'ExpressJS', icon: <SiExpress /> }, { name: 'ReactNative', icon: <TbBrandReactNative /> }, { name: 'NextJS', icon: <TbBrandNextjs /> }, { name: 'MongoDB', icon: <TbBrandMongodb /> }, { name: 'Figma', icon: <PiFigmaLogoLight /> }, { name: 'Postgress', icon: <SiPostgresql /> }, { name: 'Prisma', icon: <SiPrisma /> }]
 
-  // const bgImage = 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg';
-  // const image2 = 'https://images.pexels.com/photos/1939485/pexels-photo-1939485.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-  
-  const projects = [{name:'Shortchase', link: 'https://shortchaseclient.netlify.app/'}, 
-  // {name:'Kejepay', link:'https://kele.netlify.app/'} , 
-  {name:'Zummit', link:'https://zummitafrica.com/'}, {name:'Hallo', link:'https://www.haloasset.com/'}, 
-  // 'Oobla', 
-  // 'PX Pharma', 
-  {name:'GlobalReformation', link: 'https://globalreformationnetwork.org/'}]
-  const toolset = [{name:'Javascript', icon: <TbBrandJavascript/>}, {name:'ReactJS', icon: <FaReact/>},{name:'ExpressJS', icon: <SiExpress/>}, {name:'ReactNative', icon: <TbBrandReactNative/>}, {name: 'NextJS', icon: <TbBrandNextjs/>},{name:'MongoDB', icon: <TbBrandMongodb/>}, {name:'Figma', icon: <PiFigmaLogoLight/>}, {name:'Postgress', icon: <SiPostgresql/>}, {name:'Prisma', icon: <SiPrisma/>}]
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
 
-  // const proficiency = ['OOP', 'UX Search', 'UI Designs', 'Frontend', 'DBMS', 'Backend', 'Docker', 'Mobile App']
+  useEffect(() => {
+    const closeMenu = (e: any) => {
+      if (showMenu && !e.target.closest('.menu-container')) {
+        setShowMenu(false);
+      }
+    };
+
+    document.body.addEventListener('click', closeMenu);
+
+    return () => {
+      document.body.removeEventListener('click', closeMenu);
+    };
+  }, [showMenu]);
+
 
   return (
     <section
-    className={`relative flex w-full bg-bg h-full md:h-screen flex-col no-scrollbar scrollbar-none text-[#C5C6C7] no-scrollbar`}
-    
+      className={`relative flex w-full bg-bg h-full md:h-screen flex-col no-scrollbar scrollbar-none text-[#C5C6C7] no-scrollbar`}
+    // onClick={()=> {
+    //   setShowMenu(false)
+    // }}
     >
       <Head>
         <title> Kelechi </title>
-  
-      </Head>
-      {/* <div className="overlay"></div> */}
 
-      <div className="flex w-full justify-between items-center p-3 md:p-8">
-        <span className='md:text-6xl text-3xl  cursor-pointer transform transition-transform duration-300 hover:scale-125 '
-        onClick={()=> window.location.reload()}
+      </Head>
+
+      {/* The mobile menu */}
+      {
+        showMenu &&
+        <div className="h-screen w-[80%] max-w-[250px] absolute right-0  bg-zinc-900 transition-all duration-200 grid z-10 ">
+          <span className=' flex relative  flex-col text-lg '>
+            <span className=" absolute text-white cursor-pointer transition-all duration-300 hover:scale-125 hover:text-[#66FCF1] right-4 top-6 " onClick={(e) => {
+              e.preventDefault()
+              setShowMenu(false)
+
+            }}> <IoMdClose /> 
+            </span>
+
+            <Link href="https://github.com/kcblaq?tab=overview" target='_blank' className="flex w-full mt-20 hover:text-[#66FCF1] items-center gap-2 p-2 py-4 shadow-sm border-y  font-semibold hover:font-bold">
+              <span className="cursor-pointer  transform transition-transform duration-300 hover:scale-125" >
+                <FaGithub />
+              </span>
+              <p> Visit my Github </p>
+
+            </Link>
+            <Link href="mailto:ugwujameskelechi@gmail.com" target='_blank' className="flex w-full border-y font-semibold hover:text-[#66FCF1] items-center gap-2 p-2 py-4 shadow-sm hover:font-bold">
+              <span className="cursor-pointer  transform transition-transform duration-300 hover:scale-125" >
+                <TfiEmail />
+              </span>
+              <p> Email me</p>
+
+            </Link>
+            <Link href='https://drive.google.com/file/d/1TV_U607OCLPeX8RXdFof6hxlUZL-ZYjb/view?usp=sharing' download target='_blank' className="flex hover:font-bold w-full border-y font-semibold hover:text-[#66FCF1] items-center gap-2 p-2 py-4 shadow-sm  ">
+              <span className="cursor-pointer  transform transition-transform duration-300 hover:scale-125" >
+                <VscFilePdf />
+              </span>
+              <p> Download my CV </p>
+
+            </Link>
+            {/* <a href="https://github.com/kcblaq?tab=overview" target='_blank'>
+              <span className="cursor-pointer  transform transition-transform duration-300 hover:scale-125" >
+                <FaGithub />
+              </span>
+            </a>
+            <a href="mailto:ugwujameskelechi@gmail.com" target='_blank'>
+              <span className="cursor-pointer transform transition-transform duration-300 hover:scale-125" >
+                <TfiEmail />
+              </span>
+            </a>
+            <a href='https://drive.google.com/file/d/1TV_U607OCLPeX8RXdFof6hxlUZL-ZYjb/view?usp=sharing' download>
+
+              <span className="cursor-pointer transform transition-transform duration-300 hover:scale-125">
+                <VscFilePdf />
+              </span>
+            </a> */}
+          </span>
+        </div>
+      }
+      <div className="flex  w-full justify-between items-center p-3 md:p-8">
+
+        <span className='md:text-6xl text-3xl cursor-pointer transform transition-transform duration-300 hover:scale-125 hover:text-[#66FCF1] '
+          onClick={() => window.location.reload()}
         >
           <IoLogoAmplify />
-          {/* <img src='/logok.png' alt='Logo ' className='h-20' /> */}
         </span>
-        <span className='md:text-3xl flex gap-5 text-base items-center '>
-          <a href="https://github.com/kcblaq?tab=overview" target='_blank'>
-          <span className="cursor-pointer  transform transition-transform duration-300 hover:scale-125" >
-          <FaGithub  />
-          </span>
-          </a>
-          <a href="mailto:ugwujameskelechi@gmail.com" target='_blank'>
-          <span className="cursor-pointer transform transition-transform duration-300 hover:scale-125" >
-          <TfiEmail />
-          </span>
-          </a>
-          <a href='https://drive.google.com/file/d/1TV_U607OCLPeX8RXdFof6hxlUZL-ZYjb/view?usp=sharing' download>
 
-          <span  className="cursor-pointer transform transition-transform duration-300 hover:scale-125">
-          <VscFilePdf  />
-          </span>
-          </a>
-        </span>
-        {/* <span className='md:text-6xl text-3xl  cursor-pointer transform transition-transform duration-300 hover:scale-125 '
-        onClick={()=> window.location.reload()}
+        <span className='md:text-6xl text-3xl  cursor-pointer transform transition-transform duration-300 hover:scale-125 hover:text-[#66FCF1] '
+          onClick={(e) => {
+            e.preventDefault()
+            setShowMenu(true)
+          }}
         >
-        
-        <IoMenuSharp />
-        </span> */}
+          <IoMdMenu />
+        </span>
       </div>
 
       <div className="flex flex-col md:flex-row  w-full justify-between items-center">
         <div className='flex w-full md:w-2/3 h-full'>
-        {
-          current == 'Welcome' ? <Welcome/>
-          : current == 'Shortchase' ? <Shortchase url='https://shortchaseclient.netlify.app/'/>
-          : current =='Kejepay'? <Kejepay url='https://kejepay.com.ng/' />
-          : current == 'GlobalReformation'? <GlobalReformation url='https://globalreformationnetwork.org/'/>
-          : current == 'Zummit'? <Zummit url='https://zummitafrica.com/'/>
-          : current == 'Hallo'? <Haloasset url='https://www.haloasset.com/'/>
-          : null
-        }
+          {
+            current == 'Welcome' ? <Welcome />
+              : current == 'Shortchase' ? <Shortchase url='https://shortchaseclient.netlify.app/' />
+                : current == 'Kejepay' ? <Kejepay url='https://kejepay.com.ng/' />
+                  : current == 'GlobalReformation' ? <GlobalReformation url='https://globalreformationnetwork.org/' />
+                    : current == 'Zummit' ? <Zummit url='https://zummitafrica.com/' />
+                      : current == 'Hallo' ? <Haloasset url='https://www.haloasset.com/' />
+                        : null
+          }
         </div>
         <div className={`flex flex-col w-full md:w-1/3 h-full items-start md:p-8 p-3 text-text`}>
-        {/* <h1 className=' font-bold text-3xl flex items-center gap-2'> Projects<IoCheckbox/> </h1> */}
-        <h1 className=' font-bold text-3xl flex items-center pt-8 md:pt-0 gap-2'> Projects: </h1>
-        <div className="flex flex-wrap gap-2">
-        {
-          projects.map((project) => <p className={`${current === project.name && 'text-[#66FCF1] border-[#66FCF1]' } border transform transition-transform duration-300 hover:scale-110 font-bold cursor-pointer text-center mt-4 p-2 py-1 shadow-lg rounded-3xl  hover:text-[#66FCF1] hover:border-[#66FCF1]`} onClick={()=> setCurrent(project.name)}>{project.name} </p> )
-        }
-        </div>
+          {/* <h1 className=' font-bold text-3xl flex items-center gap-2'> Projects<IoCheckbox/> </h1> */}
+          <h1 className=' font-bold text-3xl flex items-center pt-8 md:pt-0 gap-2'> Projects: </h1>
+          <div className="flex flex-wrap gap-2">
+            {
+              projects.map((project) => <p className={`${current === project.name && 'text-[#66FCF1] border-[#66FCF1]'} border transform transition-transform duration-300 hover:scale-110 font-bold cursor-pointer text-center mt-4 p-2 py-1 shadow-lg rounded-3xl  hover:text-[#66FCF1] hover:border-[#66FCF1]`} onClick={() => setCurrent(project.name)}>{project.name} </p>)
+            }
+          </div>
+          <h1 className=' font-bold text-3xl flex items-center gap-1 mt-4'>Toolset: </h1>
+          <div className="flex flex-wrap gap-2">
+            {
+              toolset.map((project) => <p className={`border flex items-center font-bold text-center mt-4 p-2 py-1 shadow-lg `}>{project.icon} {project.name} </p>)
+            }
 
-        {/* <h1 className=' font-bold text-3xl flex items-center gap-1 mt-4'> Proficiency: </h1>
-        <div className="flex flex-wrap text-2xl gap-2">
-        {
-          proficiency.map((project) => <p className={`  font-bold flex items-center text-center mt-4 py-1 shadow-lg`}><RiCheckboxBlankFill/> {project} </p> )
-        }
-        
-        </div> */}
-        <h1 className=' font-bold text-3xl flex items-center gap-1 mt-4'>Toolset: </h1>
-        <div className="flex flex-wrap gap-2">
-        {
-          toolset.map((project) => <p className={`border flex items-center font-bold text-center mt-4 p-2 py-1 shadow-lg `}>{project.icon} {project.name} </p> )
-        }
-        
-        </div>
-        
+          </div>
+
         </div>
       </div>
     </section>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,31 +3,19 @@ import Link from "next/link";
 export default function Welcome() {
   return (
     <div className={`flex flex-col gap-4 h-full  w-full p-3 md:p-8 text-text`}>
-          <h1 className=' font-bold text-3xl md:text-6xl'> Hi, I'm Kelechi </h1>
-          <div className={`flex border-l-4 pl-6 rounded-tl-lg rounded-bl-lg`}>
-          <p className={`font-semibold text-lg `}>
-            {/* 👋 */}
-             {/* A pasionate fullstack software engineer specialized on JavaScript, React.js, React
-            Native, Express.js, and TypeScript. <br />
-            🚀 I specialize in crafting dynamic web and mobile applications, from sleek React.js UIs
-            to robust Express.js backends. <br />
-            💡 I've built a microblogging betting system for insightful predictions and dabbled in
-            e-commerce for seamless online shopping. <br />
-            🔧 With TypeScript, I ensure code reliability. Let's innovate together!
-            <br />
-            Explore my portfolio for creative and functional projects. */}
+      <h1 className=' font-bold text-3xl md:text-6xl'> Hi, I'm Kelechi </h1>
+      <div className={`flex border-l-4 pl-6 rounded-tl-lg rounded-bl-lg`}>
+        <p className={`font-semibold text-lg `}>
 
- A passionate Fullstack Software Engineer dedicated to crafting innovative and efficient solutions. With a solid foundation in both frontend and backend technologies, I bring a holistic approach to software development. My journey began with a curiosity for problem-solving, and it has evolved into a commitment to creating seamless, user-centric applications.
-          </p>
-          
-          </div>
-          <div className="pl-6 md:pt-12">
-          {/* <button className='border p-2 transform transition-transform duration-300 hover:scale-110' > Contact me
-          </button> */}
-          <div className="transform transition-transform duration-300 hover:shadow-lg">
-          <Link href={`https://calendly.com/ugwujameskelechi/30min`} className="border p-3 text-lg font-semibold transform transition-all hover:text-[#66FCF1] duration-300 hover:scale-110 hover:shadow hover:font-extrabold hover:shadow-white"> Book a meeting with me</Link>
-          </div>
-          </div>
+          A passionate Fullstack Software Engineer dedicated to crafting innovative and efficient solutions. With a solid foundation in both frontend and backend technologies, I bring a holistic approach to software development. My journey began with a curiosity for problem-solving, and it has evolved into a commitment to creating seamless, user-centric applications.
+        </p>
+
+      </div>
+      <div className="pl-6 md:pt-12">
+        <div className="transform transition-transform duration-300 hover:shadow-lg">
+          <Link href={`https://calendly.com/ugwujameskelechi/30min`} target="_blank" className="border p-3 text-lg font-semibold transform transition-all hover:text-[#66FCF1] duration-300 hover:scale-110 hover:shadow hover:font-extrabold hover:shadow-white"> Schedule a call</Link>
         </div>
+      </div>
+    </div>
   )
 }
